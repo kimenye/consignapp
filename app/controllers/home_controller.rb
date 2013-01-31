a8a804a66e845b09a7ef4965053393f6e07ba423
Trevor Kimenye <kimenye@gmail.com>
2013-01-31 18:13:26 +0300

    Added some devise views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,4 @@
 class HomeController < ApplicationController
-  def resource_name
-    :user
-  end
-
-  def resource
-    @resource ||= User.new
-  end
-
-  def devise_mapping
-    @devise_mapping ||= Devise.mappings[:user]
-  end
-
   def index
-    @users = User.all
   end
 end
